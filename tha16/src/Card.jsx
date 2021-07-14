398ae3c93aa2a67c8103be0bbc52ab59c02e935a
whereareufrom@1413 <srijavuppala11@gmail.com>
2021-07-14 17:55:33 +0530

    meme

---
+++ tha16/src/Card.jsx
@@ -0,0 +1,10 @@
+const Card =() =>{
+    return(
+        <div className="card">
+            <img src ="https://tsh.io/wp-content/uploads/2019/12/react-meme1_.png" alt=" "></img>
+            <h2>REACT CARD</h2>
+            <p>your main THA is to make meme cards</p>
+        </div>
+    )
+}
+export default Card;
---
+++ tha16/src/index.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import ReactDom from 'react-dom';
+import App from "./App.jsx";
+
+ReactDom.render( < App / > , document.getElementById("root"));
+
+//eslint-disable-next-line
+/*eslint-disable*/
